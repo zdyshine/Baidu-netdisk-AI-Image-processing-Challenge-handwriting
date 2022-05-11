13b700429b54454ae20d9ab8a83a38a3f88c026c
zdyshine <35593072+zdyshine@users.noreply.github.com>
2022-05-11 10:05:17 +0800

    Update Loss.py

--- loss/Loss.py
+++ loss/Loss.py
@@ -80,40 +80,6 @@ class LossWithGAN_STE(nn.Layer):
         holeLoss =  self.l1((1 - mask) * output, (1 - mask) * gt)
         validAreaLoss = self.l1(mask * output, mask * gt)  
         mask_loss = bce_loss(mm, 1-mask)
-        ### MSR loss ###
-        # masks_a = F.interpolate(mask, scale_factor=0.25)
-        # masks_b = F.interpolate(mask, scale_factor=0.5)
-        # imgs1 = F.interpolate(gt, scale_factor=0.25)
-        # imgs2 = F.interpolate(gt, scale_factor=0.5)
-        # msrloss = 8 * self.l1((1-mask)*x_o3,(1-mask)*gt) + 0.8*self.l1(mask*x_o3, mask*gt)+\
-        #             6 * self.l1((1-masks_b)*x_o2,(1-masks_b)*imgs2)+1*self.l1(masks_b*x_o2,masks_b*imgs2)+\
-        #             5 * self.l1((1-masks_a)*x_o1,(1-masks_a)*imgs1)+0.8*self.l1(masks_a*x_o1,masks_a*imgs1)
-
-        # feat_output_comp = self.extractor(output_comp)
-        # feat_output = self.extractor(output)
-        # feat_gt = self.extractor(gt)
-
-        # prcLoss = 0.0
-        # for i in range(3):
-        #     prcLoss += 0.01 * self.l1(feat_output[i], feat_gt[i])
-        #     prcLoss += 0.01 * self.l1(feat_output_comp[i], feat_gt[i])
-
-        # styleLoss = 0.0
-        # for i in range(3):
-        #     styleLoss += 120 * self.l1(gram_matrix(feat_output[i]),
-        #                                   gram_matrix(feat_gt[i]))
-        #     styleLoss += 120 * self.l1(gram_matrix(feat_output_comp[i]),
-        #                                   gram_matrix(feat_gt[i]))
-        """ if self.numOfGPUs > 1:
-            holeLoss = holeLoss.sum() / self.numOfGPUs
-            validAreaLoss = validAreaLoss.sum() / self.numOfGPUs
-            prcLoss = prcLoss.sum() / self.numOfGPUs
-            styleLoss = styleLoss.sum() / self.numOfGPUs """
-        # self.writer.add_scalar('LossG/Hole loss', holeLoss.item(), count)    
-        # self.writer.add_scalar('LossG/Valid loss', validAreaLoss.item(), count) 
-        # self.writer.add_scalar('LossG/msr loss', msrloss.item(), count)   
-        # self.writer.add_scalar('LossPrc/Perceptual loss', prcLoss.item(), count)    
-        # self.writer.add_scalar('LossStyle/style loss', styleLoss.item(), count)
 
         # GLoss = msrloss+ holeLoss + validAreaLoss+ prcLoss + styleLoss + 0.1 * D_fake + 1*mask_loss
         GLoss = mask_loss + holeLoss + validAreaLoss
